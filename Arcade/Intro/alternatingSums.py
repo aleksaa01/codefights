6a4634a2820d858d4153088fd2303a4d04bfd8e8
aleksaa01 <some12curious@gmail.com>
2017-11-25 22:30:57 +0100

    changing solution style

--- Arcade/Intro/alternatingSums.py
+++ Arcade/Intro/alternatingSums.py
@@ -1,3 +1,13 @@
+def alternatingSums(a):
+    even, odd = 0, 0
+    for i in range(len(a)):
+        if i % 2 == 0:
+            even += a[i]
+        else:
+            odd += a[i]
+    return [even, odd]
+
+
 """
 Several people are standing in a row and need to be divided into two teams.
 The first person goes into team 1, the second goes into team 2, the third goes
@@ -7,13 +17,9 @@ You are given an array of positive integers - the weights of the people.
 Return an array of two integers, where the first element is the total weight
 of team 1, and the second element is the total weight of team 2 after the
 division is complete.
-"""
 
-def alternatingSums(a):
-    even, odd = 0, 0
-    for i in range(len(a)):
-        if i % 2 == 0:
-            even += a[i]
-        else:
-            odd += a[i]
-    return [even, odd]
+Example
+
+For a = [50, 60, 60, 45, 70], the output should be
+alternatingSums(a) = [180, 105].
+"""
