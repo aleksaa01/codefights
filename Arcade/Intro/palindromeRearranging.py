8ef85fc53538fcf8a43ceb0fa7671d2e0cc2415a
aleksaa01 <some12curious@gmail.com>
2017-11-25 22:41:44 +0100

    changing solution style

--- Arcade/Intro/palindromeRearranging.py
+++ Arcade/Intro/palindromeRearranging.py
@@ -1,7 +1,15 @@
+def palindromeRearranging(arr):
+    return len([i for i in set(arr) if arr.count(i)%2]) < 2
+
+
 """
 Given a string, find out if its characters can be rearranged to form a
 palindrome.
-"""
 
-def palindromeRearranging(arr):
-    return len([i for i in set(arr) if arr.count(i)%2]) < 2
+Example
+
+For inputString = "aabb", the output should be
+palindromeRearranging(inputString) = true.
+
+We can rearrange "aabb" to make "abba", which is a palindrome.
+"""
