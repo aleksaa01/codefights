5f0c35b3b576300828ee38d4ada1f0682ecfd0cc
aleksaa01 <some12curious@gmail.com>
2017-11-25 22:40:49 +0100

    changing solution style

--- Arcade/Intro/arrayChange.py
+++ Arcade/Intro/arrayChange.py
@@ -1,9 +1,3 @@
-"""
-You are given an array of integers. On each move you are allowed to increase
-exactly one of its element by one. Find the minimal number of moves required
-to obtain a strictly increasing sequence from the input.
-"""
-
 def arrayChange(lst):
     res = 0
     for i in range(1, len(lst)):
@@ -13,3 +7,15 @@ def arrayChange(lst):
             lst[i] = lst[i] + sub
             res += sub
     return res
+
+
+"""
+You are given an array of integers. On each move you are allowed to increase
+exactly one of its element by one. Find the minimal number of moves required
+to obtain a strictly increasing sequence from the input.
+
+Example
+
+For inputArray = [1, 1, 1], the output should be
+arrayChange(inputArray) = 3.
+"""
