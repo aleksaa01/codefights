8a11fa7f8f2588d67df136d8f560cc676149580b
aleksaa01 <some12curious@gmail.com>
2019-01-12 11:37:55 +0100

    Challenge changed. Made new solution.

--- Arcade/Intro/reverseParentheses.py
+++ Arcade/Intro/reverseParentheses.py
@@ -1,29 +1,32 @@
-def reverseParentheses(s):
-    if '(' not in s:
-        return s
-    else:
-        i_l = s.rfind('(')
-        tmp_s = s[i_l:]
-        i_r = tmp_s.find(')')
-        new_s = tmp_s[:i_r+1]
-        new_s = new_s[1:]
-        new_s = new_s[:-1]
-        new_s = new_s[::-1]
-        new_change_string = s[:i_l] + new_s + s[i_l+i_r+1:]
-        return reverseParentheses(new_change_string)
+def reverseInParentheses(inputString):
+    cur_sequence = ''
+    stack = []
+    for i in range(len(inputString)):
+        if inputString[i] == '(':
+            stack.append(cur_sequence)
+            stack.append('(')
+            cur_sequence = ''
+        elif inputString[i] == ')':
+            while True:
+                temp = stack.pop()
+                if temp == '(':
+                    cur_sequence = cur_sequence[::-1]
+                    stack.append(cur_sequence)
+                    break
+                else:
+                    cur_sequence = temp + cur_sequence
+            cur_sequence = ''
+        else:
+            cur_sequence += inputString[i]
+
+    if len(cur_sequence) > 0:
+        stack.append(cur_sequence)
+
+    return "".join(stack)
 
 
 """
-You have a string s that consists of English letters, punctuation marks,
-whitespace characters, and brackets. It is guaranteed that the parentheses in
-s form a regular bracket sequence.
+Write a function that reverses characters in (possibly nested) parentheses in the input string.
 
-Your task is to reverse the strings contained in each pair of matching
-parentheses, starting from the innermost pair. The results string should not
-contain any parentheses.
-
-Example
-
-For string s = "a(bc)de", the output should be
-reverseParentheses(s) = "acbde".
+Input strings will always be well-formed with matching ()s.
 """
