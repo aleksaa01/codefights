7b2120d9b85d4e9f1750258f969a29484974537a
aleksaa01 <some12curious@gmail.com>
2019-01-15 07:26:32 +0100

    Added isWordPalindrome.py problem and solution.

---
+++ Arcade/Python/isWordPalindrome.py
@@ -0,0 +1,20 @@
+def isWordPalindrome(word):
+    return word == word[::-1]
+
+
+
+"""
+Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+
+Given a word, check whether it is a palindrome or not. A string is considered to be a palindrome
+if it reads the same in both directions.
+
+Example
+
+    For word = "aibohphobia", the output should be
+    isWordPalindrome(word) = true;
+
+    For word = "hehehehehe", the output should be
+    isWordPalindrome(word) = false.
+
+"""
