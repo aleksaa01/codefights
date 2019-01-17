f65bace354502c27deb11ea67da792e87613b881
aleksaa01 <some12curious@gmail.com>
2019-01-17 05:48:36 +0100

    Added repeatChar.py problem and solution.

---
+++ Arcade/Python/repeatChar.py
@@ -0,0 +1,15 @@
+repeatChar = lambda ch, n: ch*n
+
+
+"""
+Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+
+To make debug output more understandable, you often separate sets of logs by a single line of the same character. Since you use this method very often, you'd like to write a function that would handle printing the separator.
+
+Implement a function that, given a character ch and the number of times it should be repeated n, returns a string of n characters ch.
+
+Example
+
+For ch = '*' and n = 20, the output should be
+repeatChar(ch, n) = "********************".
+"""
