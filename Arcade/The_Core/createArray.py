a9287955adbe6e7fb87a32df9f1363dc50b6f127
Aleksa Avramovic <some12curious@gmail.com>
2019-02-08 21:23:18 +0100

    Added createArray.py problem and solution.

---
+++ Arcade/The_Core/createArray.py
@@ -0,0 +1,14 @@
+def createArray(size):
+    return [1] * size
+
+
+
+
+"""
+Given an integer size, return array of length size filled with 1s.
+
+Example
+
+For size = 4, the output should be
+createArray(size) = [1, 1, 1, 1].
+"""
