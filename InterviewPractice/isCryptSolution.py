9dddf4cdb845b066d1fec808024ecea0719702e4
Aleksa Avramovic <some12curious@gmail.com>
2019-02-22 17:12:44 +0100

    Added isCryptSolution.py problem and solution.

---
+++ InterviewPractice/isCryptSolution.py
@@ -0,0 +1,68 @@
+def isCryptSolution(crypt, solution):
+
+    def convert(word, d):
+        num = ""
+        for i in word:
+            num += d[i]
+        if str(int(num)) != num:
+            return False
+        return int(num)
+
+    d = {}
+    for x in solution:
+        d[x[0]] = x[1]
+
+    a = convert(crypt[0], d)
+    b = convert(crypt[1], d)
+    c = convert(crypt[2], d)
+
+    if isinstance(a, bool) or isinstance(b, bool) or isinstance(c, bool):
+        return False
+    return a + b == c
+
+
+
+"""
+A cryptarithm is a mathematical puzzle for which the goal is to find the correspondence between letters and digits, 
+such that the given arithmetic equation consisting of letters holds true when the letters are converted to digits.
+
+You have an array of strings crypt, the cryptarithm, and an an array containing the mapping of letters and digits, 
+solution. The array crypt will contain three non-empty strings that follow the structure: [word1, word2, word3], 
+which should be interpreted as the word1 + word2 = word3 cryptarithm.
+
+If crypt, when it is decoded by replacing all of the letters in the cryptarithm with digits using the mapping in 
+solution, becomes a valid arithmetic equation containing no numbers with leading zeroes, 
+the answer is true. If it does not become a valid arithmetic solution, the answer is false.
+
+Note that number 0 doesn't contain leading zeroes (while for example 00 or 0123 do).
+
+Example
+
+For crypt = ["SEND", "MORE", "MONEY"] and
+
+solution = [['O', '0'],
+            ['M', '1'],
+            ['Y', '2'],
+            ['E', '5'],
+            ['N', '6'],
+            ['D', '7'],
+            ['R', '8'],
+            ['S', '9']]
+the output should be
+isCryptSolution(crypt, solution) = true.
+
+When you decrypt "SEND", "MORE", and "MONEY" using the mapping given in crypt, you get 9567 + 1085 = 10652 
+which is correct and a valid arithmetic equation.
+
+For crypt = ["TEN", "TWO", "ONE"] and
+
+solution = [['O', '1'],
+            ['T', '0'],
+            ['W', '9'],
+            ['E', '5'],
+            ['N', '4']]
+the output should be
+isCryptSolution(crypt, solution) = false.
+
+Even though 054 + 091 = 145, 054 and 091 both contain leading zeroes, meaning that this is not a valid solution.
+"""
