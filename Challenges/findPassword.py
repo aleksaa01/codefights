05a26472bc40e009addb961922cf3bd4e1426ea0
Aleksa Avramovic <some12curious@gmail.com>
2019-03-21 10:25:47 +0100

    Added findPassword.py challenge and solution.

---
+++ Challenges/findPassword.py
@@ -0,0 +1,12 @@
+def findPassword(user, passwordList, index):
+    return passwordList[index]
+
+
+"""
+Given the username [user] and hashing list [hashingList], find the password for the username.
+
+Example:
+For user = "dcer" and passwordList = ["password", "hamper", "brewer", "Mant"] and index = 0
+
+findPassword(user, hashinglist) = "password"
+"""
