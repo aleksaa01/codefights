a7c3d14debdf75851c03fa6b7cdcb5a775fed532
Aleksa Avramovic <some12curious@gmail.com>
2019-03-21 10:12:57 +0100

    Simplified chessBoardCellColor solution.

--- Arcade/Intro/chessBoardCellColor.py
+++ Arcade/Intro/chessBoardCellColor.py
@@ -1,21 +1,5 @@
 def chessBoardCellColor(cell1, cell2):
-    d = {}
-    first_color = 'a'
-    second_color = 'b'
-    color  = None
-    for i, e in enumerate("ABCDEFGH"):
-        if i % 2 == 0:
-            color = 'a'
-        else:
-            color = 'b'
-        for i in range(1,9):
-            k = e + str(i)
-            d[k] = color
-            if color == 'a':
-                color = 'b'
-            else:
-                color = 'a'
-    return d[cell1] == d[cell2]
+    return (ord(cell1[0]) + ord(cell1[1])) % 2 == (ord(cell2[0]) + ord(cell2[1])) % 2
 
 
 """
