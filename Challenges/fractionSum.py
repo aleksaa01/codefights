f000576f0a315dc2f68b8e510e1ddfdb4fa7e88d
Aleksa Avramovic <some12curious@gmail.com>
2019-05-07 17:19:10 +0200

    Forgot to add description of fractionSum challenge.

--- Challenges/fractionSum.py
+++ Challenges/fractionSum.py
@@ -5,3 +5,15 @@ def fractionSum(a, b):
     gcd = math.gcd(den, nom)
 
     return [nom / gcd, den / gcd]
+
+
+"""
+Implement a function that can sum two reduced fractions and produce a new one.
+
+Example
+
+For a = [3, 5] and b = [7, 5], the output should be
+fractionSum(a, b) = [2, 1].
+
+3 / 5 + 7 / 5 = 10 / 5 = 2 / 1, so the answer is [2, 1]
+"""
